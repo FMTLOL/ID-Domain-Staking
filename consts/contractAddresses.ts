0450f75015b1deb48e0482aa049ee679eafb6589
FMS <71218677+FMTLOL@users.noreply.github.com>
2024-02-01 16:57:10 +0200

    Add files via upload

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0x7D853F9A29b3c317773A461ed87F54cdDa44B0e0";
+export const tokenContractAddress =
+  "0x4D3114B43fB220CE0E60C2F94495Ab8Ce9B2FA9d";
+export const stakingContractAddress =
+  "0x69a776934115375d7a65A0Cf80642625563c38D8";
